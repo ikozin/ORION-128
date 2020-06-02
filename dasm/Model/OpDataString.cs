5d5d82907a2a1048573fa4f2d16125954c49050a
Ilya Kozin <ikozin@yandex.ru>
2020-06-02 14:31:22 +0300

    update

--- dasm/Model/OpDataString.cs
+++ dasm/Model/OpDataString.cs
@@ -17,13 +17,19 @@ namespace Dasm.Model
                 if (stream.Position == stream.Length) throw new IndexOutOfRangeException();
                 byte data = (byte)stream.ReadByte();
                 if (data == 0) break;
-                if ((char)data >= ' ')
+
+                if (IsChar(data))
                     result = String.Format("{0},'{1}'", result, (char)data);
                 else
                     result = String.Format("{0},{1}", result, data);
             }
             result = result.Replace("','", "").Substring(1);
             return String.Format("{0}, 0", result);
+        }
+
+        private bool IsChar(byte data)
+        {
+            return (data >= 0x20) && (data < 0x7F);
         }
     }
 }
